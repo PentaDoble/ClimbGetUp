1ea619db31b75964142632e2ac364dfe66f6e197
GutiiXD <Gytismobil@gmail.com>
2016-04-21 09:31:26 +0200

    mejor

--- juego_guti/src/juego_guti/tienda.java
+++ juego_guti/src/juego_guti/tienda.java
@@ -1,13 +1,13 @@
 package juego_guti;
 import java.util.*;
 public class tienda {
-	public void shopselect(){
-		data llamada = new data();
+	private static Scanner teclado;
+	private static Scanner teclado2;
+	public static void shopselect(){
 		data.espacios();
 		System.out.println("Bienvenido a la tienda del badulake");
-		System.out.println("Selecciona una Opción");
-		data estadisticas = new data();
-		estadisticas.estadisticas();
+		System.out.println("Selecciona una OpciÃ³n");
+		data.estadisticas();
 		System.out.println("-------------------------------");
 		System.out.println("1. Pociones");
 		System.out.println("2. Armaduras");
@@ -15,14 +15,13 @@ public class tienda {
 		System.out.println("-------------------------------");
 		shopgo();
 	}
-	public void shopgo(){
-		Scanner teclado = new Scanner(System.in);
+	public static void shopgo(){
+		teclado2 = new Scanner(System.in);
 		try{
-			int select = teclado.nextInt();
+			int select = teclado2.nextInt();
 			switch(select){
 			case 0:
-				menu iramenu = new menu();
-				iramenu.menus2();
+				menu.menus2();
 				break;
 			case 1:
 				pociones();
@@ -36,16 +35,16 @@ public class tienda {
 			shopselect();
 		}
 	}
-	public void pociones(){
-		data llamada = new data();
+	public static void pociones(){
 		data.espacios();
 		System.out.println("1. Elixir de Vida. Restaura 25HP. Precio: 25 gold");
 		System.out.println("2. Elixir de Vida Mayor. Restaura 50HP. Precio: 50 gold ");
 		System.out.println("3. Subir vida maxima. Sube 20HP. Precio: 1000 gold");
 		System.out.println("0. Ir a la tienda");
+		pocionesselect();
 	}
-	public void pocionesselect(){
-		Scanner teclado = new Scanner(System.in);
+	public static void pocionesselect(){
+		teclado = new Scanner(System.in);
 		int select = teclado.nextInt();
 		switch(select){
 			case 0: 
@@ -59,3 +58,4 @@ public class tienda {
 		}
 	}
 }
+
