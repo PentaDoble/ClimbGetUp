eaaa31d48ae0f444dd9d2109b9eedd3958d5d9aa
kristian2oleg <kristian_oleg@hotmail.com>
2016-04-21 10:37:30 +0200

    Update codigos.java

--- juego_guti/src/juego_guti/codigos.java
+++ juego_guti/src/juego_guti/codigos.java
@@ -8,6 +8,7 @@ public class codigos {
 		private static String codigo4 = "MJDC"; //1000
 		private static String codigo5 = "CACA"; //2500
 		
+		
 		public static void codigoos(){
 			System.out.println("Ingrese un codigo.");
 			Scanner teclado = new Scanner (System.in);
@@ -15,23 +16,34 @@ public class codigos {
 				
 				if(ingresar.equals(codigo1)){
 						System.out.println("has ganado 1000 gold!");
-						codigo1 = "no valido";
+						data.oro = data.oro + 1000;
+						codigo1 = null;
+						data.espera();
 					}
 				else if(ingresar.equals(codigo2)){
 						System.out.println("has ganado 500 gold!");
-						codigo1 = "no valido";;
+						data.oro = data.oro + 500;
+						codigo2 = null;
+						data.espera();
 					}
 				else if(ingresar.equals(codigo3)){
 						System.out.println("has ganado 250 gold!");
-						codigo1 = "no valido";
+						data.oro = data.oro + 250;
+						codigo3 = null;
+						data.espera();
 					}
 				else if(ingresar.equals(codigo4)){
 						System.out.println("has ganado 1000 gold!");
-						codigo1 = "no valido";
+						data.oro = data.oro + 1000;
+						codigo4 =null;
+						data.espera();
 					}
 				else if(ingresar.equals(codigo5)){
 						System.out.println("has ganado 2500 gold!");
-						codigo1 = "no valido";
+						data.oro = data.oro + 250;
+						codigo5 = null;
+						data.espera();
 					}
+				menu.menus2();
 
 		}}
