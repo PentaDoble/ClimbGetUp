540861726ab62681c1941a6011347eaa22861d51
kristian2oleg <kristian_oleg@hotmail.com>
2016-04-27 09:17:35 +0200

    Update data.java
    Se han añadido valores :
    public static int armor = 0;
    public static int armorhp = 100;

--- juego_guti/src/juego_guti/data.java
+++ juego_guti/src/juego_guti/data.java
@@ -1,8 +1,11 @@
+package juego_guti;
 public class data {
 	public static String version = "0.0.1a";
 	public static int hp = 100;
 	public static int hpmax = 100;
 	public static int oro = 0;
+	public static int armor = 0;
+	public static int armorhp = 100;
 	
 	public static void espacios(){
 		System.out.println("");
@@ -29,7 +32,7 @@ public class data {
 	}
 	public static void estadisticas(){
 		System.out.println("-------------------------------");
-		System.out.println("Estadisticas actuales " +data.hp+ "HP/" +data.hpmax+"HP --" +data.oro+" gold");
+		System.out.println("Estadisticas actuales " +data.hp+ "HP/" +data.hpmax+"HP -- " +data.oro+" gold / Armadura = "+data.armor+ " %" );
 		System.out.println("-------------------------------");
 	}
 	public static void espera(){
