9d502871fcb384c536be12034262698ede57a44f
kristian2oleg <kristian_oleg@hotmail.com>
2016-04-27 09:49:25 +0200

    Update presentacion.java

--- juego_guti/src/juego_guti/presentacion.java
+++ juego_guti/src/juego_guti/presentacion.java
@@ -1,10 +1,25 @@
 package juego_guti;
+
 import java.util.*;
 public class presentacion {
-	public static String nombre;
 	public void introduccion(){
+		String nombreid, passid;
+		nombreid = "admin";
+		passid = "admin";
+		String nombre,pass;
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Introduce tu nombre");
+		System.out.println("Introduce tu ID");
 		nombre = teclado.nextLine();
+		System.out.println("Introduce tu Password");
+		pass = teclado.nextLine();
+		
+		if(nombre.equals(nombreid) || (pass.equals(passid))){
+			System.out.println("Inicio correcto.");
+			data.espera();
+		}else{
+			System.err.println("Error al iniciar Sesión");
+			introduccion();
+		}
+		
 	}
 }
